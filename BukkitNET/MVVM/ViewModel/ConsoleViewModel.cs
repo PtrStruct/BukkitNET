9f6ee7bfe8dfb6b2ab05307418bf68a774fdc8e6
Riley <developer@DESKTOP-SLKOSBT>
2019-06-16 12:22:15 +0200

    Continious console work

--- BukkitNET/MVVM/ViewModel/ConsoleViewModel.cs
+++ BukkitNET/MVVM/ViewModel/ConsoleViewModel.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using BukkitNET.Core;
 
 namespace BukkitNET.MVVM.ViewModel
 {
     class ConsoleViewModel : ObservableObject
     {
-
+        public RelayCommand EnterCommand { get; set; }
+        public ConsoleViewModel()
+        {
+            EnterCommand = new RelayCommand(o => { MessageBox.Show(o as string); }, o => true);
+        }
     }
 }
